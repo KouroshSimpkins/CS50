96e1feaf635863bfaa4420cc0948d7d62fb66dc1
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2021-10-18 21:08:06 +0100

    finish caeser cypher

--- Week 2 - Arrays/pset2/caeser.c
+++ Week 2 - Arrays/pset2/caeser.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 
+string cipher(int key, string plaintext);
+
 int main(int argc, string argv[])
 {
     if (argc == 2)
@@ -12,7 +14,7 @@ int main(int argc, string argv[])
         string tmp = argv[1];
         for (int i = 0; i < strlen(argv[1]); i++)
         {
-            if (atoi(& tmp[i]) == 0)
+            if ((atoi(& tmp[i]) == 0) && tmp[i] != 48)
             {
                 printf("Usage: ./caeser key\n");
                 return 1;
@@ -20,11 +22,52 @@ int main(int argc, string argv[])
         }
 
         int key = atoi(argv[1]);
-        printf("%d\n", key);
+        string plaintext = get_string("plaintext:  ");
+        string ciphertext = cipher(key, plaintext);
+        printf("ciphertext: %s\n", ciphertext);
+
     }
     else
     {
         printf("Usage: ./caeser key\n");
         return 1;
     }
+}
+
+
+string cipher(int key, string plaintext)
+{
+    string ciphertext = plaintext;
+
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        char ch = plaintext[i];
+
+        for (int j = 0; j < key; j++)
+        {
+            if (ch >= 'a' && ch <= 'z')
+            {
+                ch = ch + 1;
+
+                if (ch > 'z')
+                {
+                    ch = ch - 'z' + 'a' - 1;
+                }
+            }
+            else if (ch >= 'A' && ch <= 'Z')
+            {
+                ch = ch + 1;
+
+                if (ch > 'Z')
+                {
+                    ch = ch - 'Z' + 'A' - 1;
+                }
+            }
+
+            ciphertext[i] = ch;
+        }
+
+        printf("%c\n", ch);
+    }
+    return ciphertext;
 }
