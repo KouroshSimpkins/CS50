defb32361ce7a05cda323be5517927e6aef5b00b
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2021-11-16 16:08:32 +0000

    Working Max STR_Repeat function

--- Week 6 - Python/pset6/dna/dna.py
+++ Week 6 - Python/pset6/dna/dna.py
@@ -39,15 +39,15 @@ def STR_Compare(sequence, csv_data):
 
     # compare each value in Strs_In_Sequence with the value in csv_data
     for i in range(len(Strs_In_Sequence)):
-        for j in range(len(csv_data)):
-            if Strs_In_Sequence[i][j] != csv_data[j]:
-                break
+        if Strs_In_Sequence[i] == csv_data:
+            Max_Repeats_Temp += 1
+            if Strs_In_Sequence[i+len(csv_data)] == csv_data:
+                pass
             else:
-                Max_Repeats_Temp += 1
-                if Max_Repeats_Temp == len(csv_data):
-                    Max_Repeats += 1
-                    Max_Repeats_Temp = 0
-                    break
+                if Max_Repeats_Temp > Max_Repeats:
+                    Max_Repeats = Max_Repeats_Temp
+                Max_Repeats_Temp = 0
+    
     return Max_Repeats
 
 
@@ -55,6 +55,9 @@ def main(argv):
     """The main function of the program"""
     # The program should take 2 command line arguments:
     # a DNA sequence and a CSV file containing STR counts
+
+    Final_Str_Dictionary = {}
+
     if len(argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
         return 1
@@ -77,9 +80,15 @@ def main(argv):
         print("Usage: python dna.py data.csv sequence.txt")
         return 1
 
-    STR = csv_data[0][1]
-    print(STR)
-    print(STR_Compare(dna_sequence, STR))
+    for j in range(len(csv_data[0])):
+        if csv_data[0][j] == 'name':
+            pass
+        else:
+            Final_Str_Dictionary[csv_data[0][j]] = STR_Compare(dna_sequence, csv_data[0][j])
+
+    for word in Final_Str_Dictionary:
+        for column in csv_data:
+            print(column[0], Final_Str_Dictionary[word])
 
     return 0
 
